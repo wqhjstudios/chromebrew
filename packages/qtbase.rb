7a310207b140ac32f3091cb4a6a082f6403c71ac
Ed Reel <edreel@gmail.com>
2019-11-20 23:30:34 -0600

    Add jasper, libproxy, nss and re2c dependencies

--- packages/qtbase.rb
+++ packages/qtbase.rb
@@ -4,8 +4,8 @@ class Qtbase < Package
   description 'Qt Base (Core, Gui, Widgets, Network, ...)'
   homepage 'https://github.com/qt/qtbase'
   version '5.13.2'
-  source_url 'https://download.qt.io/official_releases/qt/5.13/5.13.2/single/qt-everywhere-src-5.13.2.tar.xz'
-  source_sha256 '55e8273536be41f4f63064a79e552a22133848bb419400b6fa8e9fc0dc05de08'
+  source_url 'https://download.qt.io/official_releases/qt/5.13/5.13.2/submodules/qtbase-everywhere-src-5.13.2.tar.xz'
+  source_sha256 '26b6b686d66a7ad28eaca349e55e2894e5a735f3831e45f2049e93b1daa92121'
 
   binary_url ({
   })
@@ -19,14 +19,18 @@ class Qtbase < Package
   depends_on 'freetds'
   depends_on 'gstreamer'
   depends_on 'harfbuzz'
+  depends_on 'jasper'
   depends_on 'jsoncpp'
-  depends_on 'pcre2'
   depends_on 'lcms'
   depends_on 'libevent'
   depends_on 'libinput'
   depends_on 'libmng'
+  depends_on 'libproxy'
   depends_on 'libvpx'
+  depends_on 'nss'
+  depends_on 'pcre2'
   depends_on 'protobuf'
+  depends_on 're2c'
 
   def self.build
     system './configure',
